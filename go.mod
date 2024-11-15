411661d549ab73dd3cd8d04ebbac777d5dd9f986
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2024-11-15 15:23:24 +0100

    inital commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fredrikkvalvik/ukenr
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	var (
+		date time.Time
+	)
+
+	flag.Parse()
+	if flag.NArg() == 1 {
+		t := flag.Arg(0)
+
+		d, err := time.Parse(time.DateOnly, t)
+		if err != nil {
+			fmt.Printf("invalid date string: %s\n", t)
+			fmt.Print(usage())
+			return
+		}
+		date = d
+	} else {
+		date = time.Now()
+	}
+
+	fmt.Print(formatDate(date))
+}
+
+func formatDate(t time.Time) string {
+	_, weekNumber := t.ISOWeek()
+
+	var s strings.Builder
+	fmt.Fprintln(&s, t.Format(time.DateOnly))
+	fmt.Fprintf(&s, "week: %d\n", weekNumber)
+
+	return s.String()
+}
+
+func usage() string {
+	var s strings.Builder
+
+	fmt.Fprintf(&s, "Usage:\n")
+	fmt.Fprintf(&s, "  %s", "ukenr\n")
+	fmt.Fprintf(&s, "  %s", "ukenr [yyyy-mm-dd]\n")
+
+	return s.String()
+}
